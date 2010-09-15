d16d561511a647e711188426f27ea450fce756eb
georg86 <georg86@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-15 15:49:34 +0000

    change eregi_replace functions to preg_replace
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sni_supportchat/trunk@38208 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_chat_functions.php
+++ lib/class.tx_chat_functions.php
@@ -70,10 +70,10 @@ class tx_chat_functions {
 	* @return String: with activated links
 	*/
 	function html_activate_links($str) {
-		$str = eregi_replace('(((f|ht){1}tp://)[-a-zA-Z0-9@:%_\+.~#?&//=]+)', '<a href="\\1" target="_blank">\\1</a>', $str);
-		$str = eregi_replace('([[:space:]()[{}])(www.[-a-zA-Z0-9@:%_\+.~#?&//=]+)', '\\1<a href="http://\\2" target="_blank">\\2</a>', $str);
-//		$str = eregi_replace('([_\.0-9a-z-]+@([0-9a-z][0-9a-z-]+\.)+[a-z]{2,3})','<a href=\"mailto:\\1\">\\1</a>', $str);
-		$str = eregi_replace('([_\.0-9a-z-]+@([0-9a-z][0-9a-z-]+\.)+[a-z]{2,3})','<a href="mailto:\\1">\\1</a>', $str);
+		$str = str_replace("http://www.","www.",$str);
+		$str = str_replace("www.","http://www.",$str);
+		$str = preg_replace("/([\w]+:\/\/[\w-?&;#~=\.\/\@]+[\w\/])/i","<a href=\"$1\" target=\"_blank\">$1</a>", $str);
+		$str = preg_replace("/([\w-?&;#~=\.\/]+\@(\[?)[a-zA-Z0-9\-\.]+\.([a-zA-Z]{2,3}|[0-9]{1,3})(\]?))/i","<a href=\"mailto:$1\">$1</a>",$str);		
 		return $str;
 	}
  

e2204c442a3b9861cecc074830bd3e6e1c4770a1
georg86 <georg86@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-08-30 15:10:46 +0000

    New ext_emconf.php for TER Release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sni_supportchat/trunk@65769 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "sni_supportchat"
+# Extension Manager/Repository config file for ext "sni_supportchat".
 #
-# Auto generated 11-01-2010 09:00
+# Auto generated 30-08-2012 17:09
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'A Support Chat for Typo3',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '1.0.4',
+	'version' => '1.1.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:53:{s:9:"ChangeLog";s:4:"5e31";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"638b";s:17:"ext_localconf.php";s:4:"c820";s:14:"ext_tables.php";s:4:"bd76";s:14:"ext_tables.sql";s:4:"a1fc";s:32:"icon_tx_snisupportchat_chats.gif";s:4:"78eb";s:35:"icon_tx_snisupportchat_messages.gif";s:4:"1e24";s:16:"locallang_db.xml";s:4:"26f3";s:7:"tca.php";s:4:"833a";s:21:"lib/class.tx_chat.php";s:4:"afa0";s:27:"lib/class.tx_chatMarket.php";s:4:"b008";s:31:"lib/class.tx_chat_functions.php";s:4:"109d";s:14:"doc/manual.sxw";s:4:"6eb6";s:21:"mod1/ajaxResponse.php";s:4:"a817";s:13:"mod1/chat.css";s:4:"81d3";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"7a37";s:14:"mod1/index.php";s:4:"5d03";s:18:"mod1/locallang.xml";s:4:"f3f5";s:22:"mod1/locallang_mod.xml";s:4:"0762";s:19:"mod1/moduleicon.gif";s:4:"638b";s:18:"mod1/img/2dots.gif";s:4:"db30";s:18:"mod1/img/close.jpg";s:4:"7f84";s:22:"mod1/img/encrypted.jpg";s:4:"b96d";s:20:"mod1/img/loading.gif";s:4:"773f";s:21:"mod1/img/title_bg.jpg";s:4:"0fbc";s:24:"mod1/img/unencrypted.jpg";s:4:"5838";s:37:"mod1/dropdown_css_img/expand_down.gif";s:4:"6e4b";s:38:"mod1/dropdown_css_img/expand_right.gif";s:4:"85e6";s:40:"mod1/dropdown_css_img/uvumi-dropdown.css";s:4:"5cf7";s:19:"js/Element.Forms.js";s:4:"2425";s:30:"js/UvumiDropdown-compressed.js";s:4:"8959";s:22:"js/flashbeep_alert.swf";s:4:"5d9c";s:28:"js/mootools-1.2.4-core-yc.js";s:4:"4651";s:27:"js/mootools-1.2.4.2-more.js";s:4:"bbde";s:25:"js/mootools.clientcide.js";s:4:"0f07";s:15:"js/prototype.js";s:4:"b568";s:21:"js/sni_supportchat.js";s:4:"a932";s:29:"js/sni_supportchatIsOnline.js";s:4:"e175";s:38:"js/sni_supportchatIsOnline_Mootools.js";s:4:"e27e";s:24:"js/sni_supportchat_be.js";s:4:"8b46";s:29:"js/sni_supportchat_be.js_sich";s:4:"e3f3";s:20:"pi1/ajaxResponse.php";s:4:"8063";s:35:"pi1/class.tx_snisupportchat_pi1.php";s:4:"bcd3";s:17:"pi1/locallang.xml";s:4:"f9bd";s:17:"pi1/template.tmpl";s:4:"d9ef";s:24:"pi1/static/editorcfg.txt";s:4:"6705";s:43:"pi1/static/Sni_SupportChat_TS/constants.txt";s:4:"d41d";s:39:"pi1/static/Sni_SupportChat_TS/setup.txt";s:4:"6120";s:16:"pics/offline.jpg";s:4:"5f6a";s:15:"pics/online.jpg";s:4:"27ab";s:21:"pics/support-logo.jpg";s:4:"9aa2";}',
+	'_md5_values_when_last_written' => 'a:60:{s:9:"ChangeLog";s:4:"66bf";s:12:"ext_icon.gif";s:4:"638b";s:17:"ext_localconf.php";s:4:"c820";s:14:"ext_tables.php";s:4:"bd76";s:14:"ext_tables.sql";s:4:"a1fc";s:32:"icon_tx_snisupportchat_chats.gif";s:4:"78eb";s:35:"icon_tx_snisupportchat_messages.gif";s:4:"1e24";s:16:"locallang_db.xml";s:4:"26f3";s:10:"README.txt";s:4:"9fa9";s:7:"tca.php";s:4:"833a";s:14:"doc/manual.sxw";s:4:"9368";s:19:"js/Element.Forms.js";s:4:"2425";s:22:"js/flashbeep_alert.swf";s:4:"5d9c";s:28:"js/mootools-1.2.4-core-yc.js";s:4:"4651";s:27:"js/mootools-1.2.4.2-more.js";s:4:"bbde";s:25:"js/mootools.clientcide.js";s:4:"0f07";s:15:"js/prototype.js";s:4:"b568";s:13:"js/smilies.js";s:4:"7079";s:21:"js/sni_supportchat.js";s:4:"4fbe";s:24:"js/sni_supportchat_be.js";s:4:"3d2b";s:29:"js/sni_supportchat_be.js_sich";s:4:"e3f3";s:29:"js/sni_supportchatIsOnline.js";s:4:"557a";s:38:"js/sni_supportchatIsOnline_Mootools.js";s:4:"b815";s:30:"js/UvumiDropdown-compressed.js";s:4:"8959";s:21:"lib/class.tx_chat.php";s:4:"af33";s:31:"lib/class.tx_chat_functions.php";s:4:"1d73";s:27:"lib/class.tx_chatMarket.php";s:4:"0ca5";s:21:"mod1/ajaxResponse.php";s:4:"c38f";s:13:"mod1/chat.css";s:4:"2517";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"7a37";s:14:"mod1/index.php";s:4:"420f";s:18:"mod1/locallang.xml";s:4:"c5a0";s:22:"mod1/locallang_mod.xml";s:4:"0762";s:19:"mod1/moduleicon.gif";s:4:"638b";s:37:"mod1/dropdown_css_img/expand_down.gif";s:4:"6e4b";s:38:"mod1/dropdown_css_img/expand_right.gif";s:4:"85e6";s:40:"mod1/dropdown_css_img/uvumi-dropdown.css";s:4:"5cf7";s:18:"mod1/img/2dots.gif";s:4:"db30";s:18:"mod1/img/close.jpg";s:4:"7f84";s:22:"mod1/img/encrypted.jpg";s:4:"b96d";s:20:"mod1/img/loading.gif";s:4:"773f";s:21:"mod1/img/title_bg.jpg";s:4:"0fbc";s:24:"mod1/img/unencrypted.jpg";s:4:"5838";s:20:"pi1/ajaxResponse.php";s:4:"90da";s:35:"pi1/class.tx_snisupportchat_pi1.php";s:4:"66d1";s:17:"pi1/locallang.xml";s:4:"c260";s:17:"pi1/template.tmpl";s:4:"d308";s:24:"pi1/static/editorcfg.txt";s:4:"6705";s:43:"pi1/static/Sni_SupportChat_TS/constants.txt";s:4:"d41d";s:39:"pi1/static/Sni_SupportChat_TS/setup.txt";s:4:"6b35";s:16:"pics/offline.jpg";s:4:"5f6a";s:15:"pics/online.jpg";s:4:"27ab";s:13:"pics/save.png";s:4:"7d94";s:21:"pics/support-logo.jpg";s:4:"9aa2";s:15:"pics/typing.png";s:4:"8926";s:19:"pics/smiley/bad.png";s:4:"5fbd";s:17:"pics/smiley/D.png";s:4:"1ee9";s:22:"pics/smiley/smiley.png";s:4:"1d73";s:20:"pics/smiley/wink.png";s:4:"56c3";}',
 	'suggests' => array(
 	),
 );
